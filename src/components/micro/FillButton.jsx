20fc019af0fa36c47c597ad2872b6bb4af069f3e
Suvro Bose <bose.suvro.com>
2023-08-20 22:03:42 +0530

    Fixed link staying in focus after clicking

--- src/components/micro/FillButton.jsx
+++ src/components/micro/FillButton.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 
 const FillButton = ({ child, href, padX, padY }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(true);
+    // Perform any other click-related actions here
+  };
+
+  const handleBlur = () => {
+    setClicked(false);
+  };
+
+
   return (
     <a
       className={`cursor-pointer tracking-wider bg-transparent border-[#F2A2E8] border-2 rounded-lg hover:bg-[#F2A2E8] hover:text-[#0a192f] font-thin transition ease-in duration-150`}
@@ -11,6 +24,8 @@ const FillButton = ({ child, href, padX, padY }) => {
         paddingBottom: `${padY}rem`,
       }}
       href={href}
+      onClick={handleClick}
+      onBlur={handleBlur}
     >
       {child}
     </a>
--- src/components/micro/SocialMediaBanner.jsx
+++ src/components/micro/SocialMediaBanner.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faLinkedin,
@@ -9,6 +9,17 @@ import {
 import { faCode } from "@fortawesome/free-solid-svg-icons";
 
 const SocialMediaIconsBanner = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(true);
+    // Perform any other click-related actions here
+  };
+
+  const handleBlur = () => {
+    setClicked(false);
+  };
+
   return (
     <div className="fixed left-1/2 md:left-6 transform -translate-x-1/2 bottom-4 md:bottom-0 md:w-fit md:inset-x-4 md:flex md:flex-col flex-row">
   <div className="flex flex-row md:flex-col p-4 space-x-6 md:space-x-0 space-y-0 md:space-y-6 md:bg-transparent bg-slate-600 bg-opacity-20 backdrop-filter backdrop-blur-md backdrop-saturate-150 md:backdrop-saturate-100 rounded-lg text-white animate-fade-up delay-1000">
@@ -21,7 +32,8 @@ const SocialMediaIconsBanner = () => {
         className="hover:scale-125 hover:text-[#F2A2E8] transition ease-in duration-150"
       />
     </a>
-    <a href="https://leetcode.com/toy0407" target="_blank" rel="noopener noreferrer">
+    <a href="https://leetcode.com/toy0407" target="_blank" rel="noopener noreferrer" onClick={handleClick}
+      onBlur={handleBlur}>
       <FontAwesomeIcon
         icon={faCode}
         size="lg"
@@ -30,7 +42,8 @@ const SocialMediaIconsBanner = () => {
         className="hover:scale-125 hover:text-[#F2A2E8] transition ease-in duration-150"
       />
     </a>
-    <a href="https://github.com/toy0407" target="_blank" rel="noopener noreferrer">
+    <a href="https://github.com/toy0407" target="_blank" rel="noopener noreferrer" onClick={handleClick}
+      onBlur={handleBlur}>
       <FontAwesomeIcon
         icon={faGithub}
         size="lg"
@@ -39,7 +52,8 @@ const SocialMediaIconsBanner = () => {
         className="hover:scale-125 hover:text-[#F2A2E8] transition ease-in duration-150"
       />
     </a>
-    <a href="https://twitter.com/toy_0407" target="_blank" rel="noopener noreferrer">
+    <a href="https://twitter.com/toy_0407" target="_blank" rel="noopener noreferrer" onClick={handleClick}
+      onBlur={handleBlur}>
       <FontAwesomeIcon
         icon={faTwitter}
         size="lg"
@@ -48,7 +62,8 @@ const SocialMediaIconsBanner = () => {
         className="hover:scale-125 hover:text-[#F2A2E8] transition ease-in duration-150"
       />
     </a>
-    <a href="https://www.instagram.com/toy0407" target="_blank" rel="noopener noreferrer">
+    <a href="https://www.instagram.com/toy0407" target="_blank" rel="noopener noreferrer" onClick={handleClick}
+      onBlur={handleBlur}>
       <FontAwesomeIcon
         icon={faInstagram}
         size="lg"
