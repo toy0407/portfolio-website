a77f0456e88c57ba11cd2e3ba02e922b0cac419a
Suvro Bose <bose.suvro.com>
2024-02-17 19:15:04 +0530

    Disabled project section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Navbar from '../../src/components/Navbar'
-import Main from '../../src/components/Main'
-import About from '../../src/components/About'
-import Skills from '../../src/components/Skills'
-import Contact from '../../src/components/Contact'
-import Projects from '../../src/components/Projects'
-import WorkExperience from '../../src/components/WorkExperience'
-import SocialMediaIconsBanner from '@/components/micro/SocialMediaBanner'
-import ScrollToTopButton from '@/components/micro/ScrollToTopButton'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Navbar from "../../src/components/Navbar";
+import Main from "../../src/components/Main";
+import About from "../../src/components/About";
+import Skills from "../../src/components/Skills";
+import Contact from "../../src/components/Contact";
+import Projects from "../../src/components/Projects";
+import WorkExperience from "../../src/components/WorkExperience";
+import SocialMediaIconsBanner from "@/components/micro/SocialMediaBanner";
+import ScrollToTopButton from "@/components/micro/ScrollToTopButton";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
   return (
-    <div className='snap-y snap-mandatory'>
+    <div className="snap-y snap-mandatory">
       <Head>
         <title>Suvro's Portfolio</title>
         <meta name="description" content="Suvro Bose Portfolio Website" />
@@ -26,19 +25,28 @@ export default function Home() {
 
       <Navbar />
 
-      <section><Main /></section>
+      <section>
+        <Main />
+      </section>
 
-      <section id="about" className='snap-start'><About /></section>
-      <section id="skills" className='snap-start'><Skills /></section>
-      <section id="projects" className='snap-start'><Projects /></section>
-      <section id="work" className='snap-start'><WorkExperience /></section>
+      <section id="about" className="snap-start">
+        <About />
+      </section>
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
+      {/* <section id="projects" className='snap-start'><Projects /></section> */}
+      <section id="work" className="snap-start">
+        <WorkExperience />
+      </section>
 
       {/* <Testimonials/> */}
       {/* <Collaborations/> */}
-      <section id="contact" className='snap-center'><Contact /></section>
+      <section id="contact" className="snap-center">
+        <Contact />
+      </section>
       <SocialMediaIconsBanner />
       <ScrollToTopButton />
     </div>
-
-  )
+  );
 }
